b917dde0041ec5fa025eaa6246c8d590068ab086
loethor <lrb2392@gmail.com>
2023-05-15 08:19:44 +0200

    Bomb chain detonation implemented

--- scenes/characters/bomb.gd
+++ scenes/characters/bomb.gd
@@ -5,6 +5,7 @@ var bomb_power = 1
 
 @onready var collision := $StaticBody3D/CollisionShape3D
 @onready var bomb_area_of_efect = []
+var is_exploding = false
 
 func _prepare_area_of_effect(bomb_power):
 	bomb_area_of_efect.append(Vector2(global_position.x, global_position.z))
@@ -15,10 +16,19 @@ func _prepare_area_of_effect(bomb_power):
 		bomb_area_of_efect.append(Vector2(global_position.x, global_position.z - TILE_SIZE * i))
 
 func _ready() -> void:
+#	print("My name is %s "% name)
+	Signals.connect("has_exploded", _check_bomb_explosion)
 	_prepare_area_of_effect(bomb_power)
+
+func _check_bomb_explosion(explosion_position: Vector2):
+	var my_2d_position = Vector2(position.x, position.z)
+	if (my_2d_position == explosion_position) and not is_exploding:
+		explode()
+
 
 func explode() -> void:
 	print("BOOM")
+	is_exploding = true
 	for targeted_area in bomb_area_of_efect:
 		Signals.emit_signal("has_exploded", targeted_area)
 	queue_free()
