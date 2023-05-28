8f0c19a381bdf203c06e2a2443eef21b83ad4f21
loethor <lrb2392@gmail.com>
2023-05-28 20:35:16 +0200

    procedural map is now parametriced

--- game/main_menu.gd
+++ game/main_menu.gd
@@ -11,40 +11,5 @@ func _on_single_player_button_pressed() -> void:
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
-
-# multiplayer
-func _ready():
-	# Start the server if Godot is passed the "--server" argument.
-	# This lets us host the server headless if we want :)
-	if "--server" in OS.get_cmdline_args():
-		_on_host_button_pressed.call_deferred()
-
-
-func _on_host_button_pressed() -> void:
-	NetworkState.start_network(true)
-	load_scene()
-
-func _on_join_button_pressed() -> void:
-	NetworkState.start_network(false, ip_input.text, port_input.text.to_int())
-	load_scene()
-
 func load_scene():
 	get_tree().change_scene_to_packed(map_scene)
-
-
-
-
-#func add_player(peer_id):
-#	var player = Player.instantiate()
-#	player.name = str(peer_id)
-#	var spawn_location = level.obtain_spawn()
-#	print("Spawn location of player %s: %s" % [peer_id, spawn_location])
-#	player.global_position = spawn_location
-#	players.add_child(player)
-#
-#func remove_player(peer_id):
-#	var player = get_node_or_null(str(peer_id))
-#	if player: player.queue_free()
-
-
-
--- scenes/levels/procedural.gd
+++ scenes/levels/procedural.gd
@@ -5,9 +5,15 @@ var coordinate = FastNoiseLite.new()
 var width = 40
 var height = 40
 @onready var player = $"../TestPlayer"
+@export var seed :int
+
+@export_range(1, 10, .2) var noise_scale: float = 1
+@export_range(0.3, 0.4, 0.01) var crumble_limit: float = 0.3
+@export_range(0.6, 0.8, 0.01) var block_limit: float = 0.6
 
 func _ready() -> void:
-	coordinate.seed = randi()
+#	coordinate.seed = randi()
+	coordinate.seed = seed
 #	for i in range(-width,width):
 #		for j in range(-height, height):
 #			var cell_pos = Vector3i(i, 0, j)
@@ -25,12 +31,12 @@ func generate_chunk(pos):
 	for i in range(-width,width):
 		for j in range(-height, height):
 			var cell_pos = Vector3i(tile_pos.x + i, 0, tile_pos.z + j)
-			var coord = (coordinate.get_noise_2d(cell_pos.x, cell_pos.z) + 1)/2
+			var coord = (coordinate.get_noise_2d(cell_pos.x * noise_scale, cell_pos.z * noise_scale) + 1)/2
 #			print(coord)
 #			print(floor(coor.d))
-			if coord >= 0 and coord < 0.3:
+			if coord >= 0 and coord < crumble_limit:
 				set_cell_item(cell_pos, 1)
-			elif coord >= 0.4 and coord < 0.6:
+			elif coord >= crumble_limit and coord < block_limit:
 				set_cell_item(cell_pos, 0)
 			else:
 				set_cell_item(cell_pos, 2)
