920f1870f69326b9731f85c84c721849c0930019
loethor <lrb2392@gmail.com>
2023-05-31 21:47:43 +0200

    Added bomb timer label

--- items/bomb.gd
+++ items/bomb.gd
@@ -6,6 +6,7 @@ const TILE_SIZE = 1
 @onready var bomb_area_of_efect = []
 @onready var explosion_sound: AudioStreamPlayer3D = $explosion_sound
 @onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var timer_label:Label3D = $time_label
 
 var is_exploding = false
 
@@ -18,11 +19,17 @@ func _prepare_area_of_effect() -> void:
 		bomb_area_of_efect.append(Vector2(global_position.x, global_position.z - TILE_SIZE * i))
 
 func _ready() -> void:
+	_update_time_label()
 	_prepare_area_of_effect()
 	anim.play("bombAction")
+	await get_tree().create_timer(1)
+
+func _process(delta: float) -> void:
+	_update_time_label()
 
 func explode() -> void:
 	if not is_exploding:
+		timer_label.hide()
 		is_exploding = true
 		_supress_particles()
 		for targeted_area in bomb_area_of_efect:
@@ -48,3 +55,6 @@ func _supress_particles() -> void:
 	var fire_particles: GPUParticles3D = $FireParticles
 	fire_particles.emitting = false
 	fire_particles.hide()
+
+func _update_time_label():
+	timer_label.text = str(ceil($Timer.time_left))
